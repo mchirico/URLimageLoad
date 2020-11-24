4d328009d4964902d6eb4404d58a5cb66b5b6ded
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-24 12:23:23 -0500

    Initial Commit

---
+++ URLimageLoad/URLimageLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLimageLoadApp.swift
+//  URLimageLoad
+//
+//  Created by Mike Chirico on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct URLimageLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
